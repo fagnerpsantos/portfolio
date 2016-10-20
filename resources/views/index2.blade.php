cb93b9b9fcebfc65cd7daf95a56840d273177a01
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2016-10-20 17:15:52 -0300

    First Commit

---
+++ resources/views/index2.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+
+    <title>Fagner Pinheiro - Portfolio </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/main.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/hover.zoom.js"></script>
+    <script src="js/hover.zoom.conf.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Static navbar -->
+    <div class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">FAGNER</a>
+        </div>
+        <!-- Static navbar
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="work.html">Work</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </div>-->
+      </div>
+    </div>
+
+	<!-- +++++ Welcome Section +++++ -->
+	<div id="ww">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 centered">
+					<img src="img/user.png" alt="Stanley">
+					<h1>Hi, I am Fagner!</h1>
+					<p>Hello everybody. I'm Fagner, I'm a student of Information System at Bahia Federal Institute of Education, Science and Technology, Web Developer Frellancer at <a href="http://mrbool.com">Mr.Bool</a> and Editorial Reviewer at <a href="http://www.devmedia.com.br">DevMedia</a>.</p>
+				
+				</div><!-- /col-lg-8 -->
+			</div><!-- /row -->
+	    </div> <!-- /container -->
+	</div><!-- /ww -->
+	
+	
+	<!-- +++++ Projects Section +++++ -->
+	
+	<div class="container pt">
+	<h1 align="center">My Portfolio</h1>
+	<hr>
+		<div class="row mt centered">	
+			<div class="col-lg-4">
+				<a class="zoom green" href="http://dochelpplus.herokuapp.com/login" target="__blank"><img class="img-responsive" src="img/portfolio2/port01.jpg" alt="" /></a>
+				<p>DocHelp+</p>
+			</div>
+			<div class="col-lg-4">
+				<a class="zoom green" href="http://splarch.herokuapp.com" target="__blank"><img class="img-responsive" src="img/portfolio2/port02.jpg" alt="" /></a>
+				<p>SPLArch</p>
+			</div>
+
+		</div><!-- /row -->
+	</div><!-- /container -->
+	
+	
+	<!-- +++++ Footer Section +++++ -->
+	
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4">
+					<h4>My Bunker</h4>
+					<p>
+						Far Far Away,<br/>
+						+55 (77)99174 1707, <br/>
+						Vitória da Conquista - BA, Brazil.
+					</p>
+				</div><!-- /col-lg-4 -->
+				
+				<div class="col-lg-4">
+					<h4>My Links</h4>
+					<p>
+						<a href="https://www.facebook.com/FagnerPinheiroSantos">Facebook</a><br/>
+						<a href="https://www.linkedin.com/in/fagner-pinheiro-9636aabb?trk=hp-identity-name">LinkedIn</a><br/>
+						<a href="https://github.com/fagnerpsantos">GitHub</a>
+					</p>
+				</div><!-- /col-lg-4 -->
+				
+				<div class="col-lg-4">
+					<h4>About Fagner</h4>
+					<p>Some things.</p>
+				</div><!-- /col-lg-4 -->
+			
+			</div>
+		
+		</div>
+	</div>
+	
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
